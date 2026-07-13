b5e2e185dd53d4df039cd5bec9ae913b0ee9cd58
Jikan Deploy <deploy@jikan.local>
2026-07-13 13:05:17 +0000

    Suppress PHP 8.0 deprecation warnings for Lumen 5.8

--- public/index.php
+++ public/index.php
@@ -5,6 +5,9 @@
 |--------------------------------------------------------------------------
 */
 
+// Suppress PHP 8.0 deprecation warnings for Lumen 5.8 compatibility
+error_reporting(E_ALL & ~E_DEPRECATED);
+
 $app = require __DIR__.'/../bootstrap/app.php';
 
 ob_start("ob_gzhandler");
